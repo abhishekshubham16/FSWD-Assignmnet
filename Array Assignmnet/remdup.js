cb7839668c99a1cba4569578fa3003986a335f66
abhishekshubham16 <abhishekshubham087@gmail.com>
2021-04-06 12:05:28 +0530

    new

---
+++ Array Assignmnet/remdup.js
@@ -0,0 +1,11 @@
+function getUnique(arr) {
+    let uniqueArr = [];
+    for (let i of arr) {
+      if (uniqueArr.indexOf(i) === -1) {
+        uniqueArr.push(i);
+      }
+    }
+    console.log(uniqueArr);
+  }
+  const array = [1, 2, 3, 2, 3, 5, 6, 5, 6];
+  getUnique(array);
